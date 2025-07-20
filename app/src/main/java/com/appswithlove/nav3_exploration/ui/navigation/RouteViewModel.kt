130b340180b39b40a0e781dafaf3207332e05e34
Yannick Pulver <yannick@appswithlove.com>
2025-07-20 17:03:08 +0200

    fix: Use default false instead of nullable boolean state
    Replace getBooleanOrNullFlow with getBooleanFlow providing false as
    default to avoid nullable state and simplify state handling in ViewModel

--- app/src/main/java/com/appswithlove/nav3_exploration/ui/navigation/RouteViewModel.kt
+++ app/src/main/java/com/appswithlove/nav3_exploration/ui/navigation/RouteViewModel.kt
@@ -5,13 +5,13 @@ import androidx.lifecycle.viewModelScope
 import com.appswithlove.nav3_exploration.ui.login.Login
 import com.russhwolf.settings.ExperimentalSettingsApi
 import com.russhwolf.settings.ObservableSettings
-import com.russhwolf.settings.coroutines.getBooleanOrNullFlow
+import com.russhwolf.settings.coroutines.getBooleanFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
 
 class RouteViewModel(settings: ObservableSettings) : ViewModel() {
 
     @OptIn(ExperimentalSettingsApi::class)
-    val state = settings.getBooleanOrNullFlow(Login.key)
+    val state = settings.getBooleanFlow(Login.key, false)
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000), null)
 }
